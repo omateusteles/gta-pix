6634f6e95424e9476d9e406323fe2e8e71066038
Mateus T. Catarino <mateustelescatarino@gmail.com>
2025-05-17 16:03:28 -0300

    refactor: indentação

--- api/app.js
+++ api/app.js
@@ -1,63 +1,65 @@
-const axios = require('axios');
+const axios = require("axios");
 
 //#region DEBUG
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 (async () => {
-  const valor = await ask('Digite o valor (em reais): ')
-        .then(v => parseFloat(v.replace(',', '.')));
-  const chavePix = await ask('Digite a chave Pix: ')
+    const value = await ask("Digite o valor (em reais): ")
+        .then(v => parseFloat(v.replace(",", ".")));
+    const pixAddressKey = await ask("Digite a chave Pix: ")
         .then(k => k.trim());
     const access_token = await ask("Digite a chave de API da conta Asaas: ")
         .then((k) => k.trim());
 
-  try {
-    const { success, errorMessage, statusCode } = await sendPix(valor, chavePix, access_token);
-    if (success) {
-      console.log('✅ Transferência criada com sucesso!');
-    } else {
-      console.log(`⚠️  Falhou (${statusCode}): ${errorMessage}`);
+    try {
+        const { success, errorMessage, statusCode } = await sendPix(value, pixAddressKey, access_token);
+        if (success) {
+            console.log("✅ Transferência criada com sucesso!");
+        } else {
+            console.log("⚠️  Falhou (${statusCode}): ${errorMessage}");
+        }
+    } catch (err) {
+        console.error("❌ Erro inesperado:", err);
     }
-  } catch (err) {
-    console.error('❌ Erro inesperado:', err);
-  }
 
-  rl.close();
+    rl.close();
 })();
 
 function ask(pergunta) {
-  return new Promise(res => rl.question(pergunta, res));
+    return new Promise((res) => rl.question(pergunta, res));
 }
 //#endregion
 
 async function sendPix(value, pixAddressKey, access_token) {
-  try {
-    const response = await axios({
-      method: 'POST',
-      url: 'https://api.asaas.com/v3/transfers',
-      headers: {
-        'Content-Type': 'application/json',
-        'access_token': access_token
-      },
-      validateStatus: status => status >= 200 && status < 500,
-      data: {
-        value: value,
-        pixAddressKey: pixAddressKey
-      }
-    });
+    try {
+        const response = await axios({
+            method: "POST",
+            url: "https://api.asaas.com/v3/transfers",
+            headers: {
+                "Content-Type": "application/json",
+                "access_token": access_token
+            },
+            validateStatus: (status) => status >= 200 && status < 500,
+            data: {
+                value: value,
+                pixAddressKey: pixAddressKey
+            }
+        });
 
-    const { status, data } = response;
-    const success = data?.success ?? (status >= 200 && status < 300);
-    const errorMessage = success ? null : data?.errors[0].description || 'Erro desconhecido';
+        const { status, data } = response;
+        const success = data?.success ?? (status >= 200 && status < 300);
 
-    return { success, errorMessage, statusCode: status };
+        const errorMessage = success
+            ? null
+            : data?.errors[0].description || "Erro desconhecido";
 
-  } catch (error) {
-    return { success: false, errorMessage: error.message, statusCode: 500 };
-  }
+        return { success, errorMessage, statusCode: status };
+    } catch (error) {
+        return { success: false, errorMessage: error.message, statusCode: 500 };
+    }
 }
