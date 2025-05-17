42f734c7457589c9b678f825b28883df4827eaef
lucassandes <lucaasdavid8@gmail.com>
2025-05-17 13:45:40 -0300

    criando views

---
+++ MobileApp/Views/WinnerPendingView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSorteiOS.Views;
+
+public partial class WinnerPendingView : ContentView
+{
+    public WinnerPendingView()
+    {
+        InitializeComponent();
+    }
+}
