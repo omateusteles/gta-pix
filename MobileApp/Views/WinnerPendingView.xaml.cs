2c7b63f0f66b02e889135192250425e412f6054e
lucassandes <lucaasdavid8@gmail.com>
2025-05-17 14:21:27 -0300

    corrigindo nome do projeto

--- MobileApp/Views/WinnerPendingView.xaml.cs
+++ MobileApp/Views/WinnerPendingView.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppSorteiOS.Views;
+namespace MobileApp.Views;
 
 public partial class WinnerPendingView : ContentView
 {
