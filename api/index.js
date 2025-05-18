1606a1b71b3eff0f289d7dee134667dd8c0ec980
Pedro Henrique de Medeiros <pedro.medeiros@asaas.com.br>
2025-05-18 10:03:14 -0300

    feat: create sendPix and authentication endpoints; create asaas-manager

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
+import AsaasManager from './managers/asaas-manager.js'
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
 
@@ -9,11 +10,42 @@ app.post('/authorize', (req, res) => {
     res.status(200).json({ status: 'APPROVED' });
 });
 
+app.post('/sendPix', async (req, res) => {
+    try {
+        const { value, pixAddressKey } = req.body;
 
-app.post('/sendPix', (req, res) => {
+        const asaasManager = new AsaasManager();
+
+        const { success, errorMessage, statusCode } = await asaasManager.sendPix(value, pixAddressKey, req.get('access_token'))
+
+        const payload = { success };
+
+        if (errorMessage) {
+            payload.errorMessage = errorMessage;
+        }
+
+        return res.status(statusCode).json(payload);
+    } catch (error) {
+        return res.status(500).json({ success: false, errorMessage: "Ocorreu um erro desconhecido. Se fodeu" });
+    }
 });
 
-app.post('/authentication', (req, res) => {
+app.get('/authentication', async (req, res) => {
+    const asaasManager = new AsaasManager();
+
+    const { success, errorMessage, statusCode, balance } = await asaasManager.getBalance(req.get('access_token'))
+
+    const payload = { success };
+
+    if (!success && errorMessage) {
+        payload.errorMessage = errorMessage;
+    }
+
+    if (balance) {
+        payload.balance = balance
+    }
+
+    return res.status(statusCode).json(payload);
 });
 
 app.listen(PORT, () => {
